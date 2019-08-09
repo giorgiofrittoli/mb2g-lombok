7d266e54644d72e30e6c587625452b7925177cb3
frigir <giorgio.frittoli@gmail.com>
2019-08-09 11:07:58 +0200

    lombok prj

---
+++ src/main/java/it/frigir/model/Person.java
@@ -0,0 +1,18 @@
+package it.frigir.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
